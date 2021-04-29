1df2ccb83aeb67d663096830f6176edb2f85be1b
Joonas Halapuu <joonas.halapuu16@gmail.com>
2021-04-29 17:17:16 +0300

    Added basic sunset and sunrise calculations.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function init(){
             zoom: 2,
             projection: 'EPSG:4326',//Coordinates in x = longitude and y = latitude, 
             maxZoom: 20,
-            extent: [-180,-90,180,90] // Map cannot go out of bounds
+            extent: [-180,-90,180,90] // Map cannot go out of bounds.
         }),
         layers: [
             new ol.layer.Tile({
@@ -20,10 +20,25 @@ function init(){
     map.on('click', function(e){
         let longitudeField = document.getElementById('longitude');
         let latitudeField = document.getElementById('latitude');
-        longitudeField.value=e.coordinate[0]
-        latitudeField.value=e.coordinate[1]
+        longitudeField.value=e.coordinate[0];
+        latitudeField.value=e.coordinate[1];
+
+
+        let sunriseField = document.getElementById("sunriseTime");
+        let sunseteField = document.getElementById("sunsetTime");
+        let dayLengthField = document.getElementById("lengthOfDay");
+        let yearMonthDay = document.getElementById("calendarDate").value.split("-");
+        //Date format is (YYYY,MM,DD) where jan is 0 and dec is 11.
+        let times = SunCalc.getTimes(new Date(yearMonthDay[0],(yearMonthDay[1]-1),yearMonthDay[2]), latitudeField.value, longitudeField.value);
+        sunriseField.innerHTML=times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
+        sunseteField.innerHTML=times.sunset.getHours() + ':' + times.sunset.getMinutes();
+        dayLengthField.innerHTML = (times.sunset.getHours()-times.sunrise.getHours())+ ':' +times.sunset.getMinutes();
     })
 
+    //Default date is today.
     var today = moment().format('YYYY-MM-DD');
     document.getElementById("calendarDate").value = today;
+
+    
+
 }
