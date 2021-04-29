eb1c50c0754640e2f23910c33d63600b1b623a3a
Joonas Halapuu <joonas.halapuu16@gmail.com>
2021-04-29 23:01:34 +0300

    Added nonfunctional line graph + some functions for calculating day length.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 window.onload = init
 
-function init(){
+function init(){ 
+    //Map
     const map = new ol.Map({
         view: new ol.View({
             center: [0,0],
@@ -24,10 +25,15 @@ function init(){
         latitudeField.value=e.coordinate[1];
 
 
+        //TODO: Put as much into functions as possible.
+
+        //let longitude = document.getElementById('longitude').value;
+        //let latitude = document.getElementById('latitude').value;
+        //CALCULATE SUNRISE AND SUNSET
         let sunriseField = document.getElementById("sunriseTime");
         let sunseteField = document.getElementById("sunsetTime");
         let dayLengthField = document.getElementById("lengthOfDay");
-        let yearMonthDay = document.getElementById("calendarDate").value.split("-");
+        let yearMonthDay = document.getElementById("startDate").value.split("-");
         //Date format is (YYYY,MM,DD) where jan is 0 and dec is 11.
         let times = SunCalc.getTimes(new Date(yearMonthDay[0],(yearMonthDay[1]-1),yearMonthDay[2]), latitudeField.value, longitudeField.value);
         sunriseField.innerHTML=times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
@@ -35,10 +41,107 @@ function init(){
         dayLengthField.innerHTML = (times.sunset.getHours()-times.sunrise.getHours())+ ':' +times.sunset.getMinutes();
     })
 
-    //Default date is today.
+
+    //TODO:
+    //IDEA! When datesArray.length<30 -> show all dates separately
+    //datesArray.length>30 and <75 -> show every third date on x axis
+    // When >75 and less than 1 year show every month
+    //Then show every second month 
+    //Then show years.
+    //Given: latitude and longitude numbers and an array of dates to calculate day lengths for.
+    //Return:  array of numbers where each represents the days length of given date respectively.
+    var calculateDayLengths= function(latitude, longitude, datesArray){
+        let x=[];
+        for (i=0; i<datesArray.length; i++){
+            let date = datesArray[i].value.split("-");
+            let times = SunCalc.getTimes(new Date(date[0],(date[1]-1),date[2]), latitude, longitude);
+            x.push(dayLength(times.sunrise, times.sunset));
+        }
+        return x;
+    };
+
+    //Given: sunrise and sunset of Suncalc times getTimes function.
+    //Return: correctly calculated length of the day rounded up to minutes.
+    var dayLength= function (sunrise, sunset){
+        let riseHours = sunrise.getHours()
+        let riseMinutes = sunrise.getMinutes()
+        let setHours = sunset.getHours()
+        let setMinutes = sunset.getMinutes()
+
+        //TODO CALCULATE CORRECTLY!
+    }
+
+    //Set default date to today.
     var today = moment().format('YYYY-MM-DD');
-    document.getElementById("calendarDate").value = today;
+    document.getElementById("startDate").value = today;
 
+    //Chart Data
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+      ];
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Day length in hours',
+            fill: true,
+            lineTension: 0.2,
+            backgroundColor: 'rgb(255, 192, 203)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }],
+    };
+
+    const config = {
+        type: 'line',
+        data,
+        options: {
+            scales: {
+                y: {
+                    type: 'linear',
+                    min: 0,
+                    max: 24
+                }
+            }
+        }
+    };
+
+    //Actual chart
+    var myChart = new Chart(
+        document.getElementById('lineChart'),
+        config
+    );
+
+
+    //Must be refractored into proper function and added to "startDate.onchange" as well.
+    document.getElementById("endDate").onchange = function(){
+        // print out all dates between given time.
+        let startDate = document.getElementById("startDate").value;
+        let endDate = document.getElementById("endDate").value;
+
+        var dateList = getDaysBetweenDates(startDate, endDate);
+        console.log(dateList);
+    };
     
-
 }
+
+//Finds all dates between given two dates.
+//Given: startDate and endDate.
+//Return: array of dates in MM/DD/YYYY and in chronological order.
+//Credit: https://www.itsolutionstuff.com/post/get-all-dates-between-two-dates-in-moment-jsexample.html
+var getDaysBetweenDates = function(startDate, endDate) {
+    startDate=moment(startDate)
+    endDate=moment(endDate)
+    var now = startDate.clone(), dates = [];
+    
+    while (now.isSameOrBefore(endDate)) {
+        dates.push(now.format('MM/DD/YYYY'));
+        now.add(1, 'days');
+    }
+    return dates;
+};
+
